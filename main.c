263c8463518e6608bdf1a31f2a7a823aeff50311
Eimrine's Point of View <eimrine@users.noreply.github.com>
2023-02-10 15:05:09 +0200

    Update main.c
    ZhekaMQ does not need this function

--- main.c
+++ main.c
@@ -259,39 +259,4 @@ void delete_end()
                 free(ptr);
         }
 }
-void delete_pos()
-{
-        int i,pos;
-        struct node *temp,*ptr;
-        if(start==NULL)
-        {
-                printf("\nThe List is Empty:\n");
-                exit(0);
-        }
-        else
-        {
-                printf("\nEnter the position of the node to be deleted:\t");
-                scanf("%d",&pos);
-                if(pos==0)
-                {
-                        ptr=start;
-                        start=start->next ;
-                        printf("\nThe deleted element is:%d\t",ptr->info  );
-                        free(ptr);
-                }
-                else
-                {
-                        ptr=start;
-                        for(i=0;i<pos;i++) { temp=ptr; ptr=ptr->next ;
-                                if(ptr==NULL)
-                                {
-                                        printf("\nPosition not Found:\n");
-                                        return;
-                                }
-                        }
-                        temp->next =ptr->next ;
-                        printf("\nThe deleted element is:%d\t",ptr->info );
-                        free(ptr);
-                }
-        }
-}
+
