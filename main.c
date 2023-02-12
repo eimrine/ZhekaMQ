31d5041f4eec233c2d2edf7c074e4a0b7f828bb8
eimrine <eimrine@gmail.com>
2023-02-12 20:19:58 +0200

    i need to fix these from loops or nothing good will happen

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ void display();
 void insert_begin();
 void insert_end();
 void insert_pos();
+void delete_pos();
 void delete_begin();
 void delete_end();
 void insert_nonce_master();
@@ -265,4 +266,76 @@ struct node *next_master(struct node *remember_this_pointer){
         struct node *temp;
         temp->next_master = remember_this_pointer->next_master;
         return temp;
+}
+
+void insert_pos()
+{
+        struct node *ptr,*temp;
+        int i,pos;
+        temp=(struct node *)malloc(sizeof(struct node));
+        if(temp==NULL)
+        {
+                printf("\nOut of Memory Space:\n");
+                return;
+        }
+        printf("\nEnter the position for the new node to be inserted:\t");
+        scanf("%d",&pos);
+        printf("\nEnter the data value of the node:\t");
+        scanf("%d",&temp->info) ;
+
+        temp->next=NULL;
+        if(pos==0)
+        {
+                temp->next=start;
+                start=temp;
+        }
+        else
+        {
+                for(i=0,ptr=start;i<pos-1;i++) { ptr=ptr->next;
+                        if(ptr==NULL)
+                        {
+                                printf("\nPosition not found:[Handle with care]\n");
+                                return;
+                        }
+                }
+                temp->next =ptr->next ;
+                ptr->next=temp;
+        }
+}
+
+void delete_pos()
+{
+        int i,pos;
+        struct node *temp,*ptr;
+        if(start==NULL)
+        {
+                printf("\nThe List is Empty:\n");
+                exit(0);
+        }
+        else
+        {
+                printf("\nEnter the position of the node to be deleted:\t");
+                scanf("%d",&pos);
+                if(pos==0)
+                {
+                        ptr=start;
+                        start=start->next ;
+                        printf("\nThe deleted element is:%d\t",ptr->info  );
+                        free(ptr);
+                }
+                else
+                {
+                        ptr=start;
+                        for(i=0;i<pos;i++) { temp=ptr; ptr=ptr->next ;
+                                if(ptr==NULL)
+                                {
+                                        printf("\nPosition not Found:\n");
+                                        return;
+                                }
+                        }
+                        temp->next =ptr->next ;
+                        printf("\nThe deleted element is:%d\t",ptr->info );
+                        free(ptr);
+                }
+        }
 }
