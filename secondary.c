25b5dc838ca96ea2f06913e62c1a07a794c360fc
Eimrine's Point of View <eimrine@users.noreply.github.com>
2023-03-08 18:44:50 +0200

    Update secondary.c
    double-linked list is working! 
    i can display it either from tail or from head!!!
    only pop() is yet to be implemented

--- secondary.c
+++ secondary.c
@@ -22,6 +22,7 @@ void  dummy_deque_push();
 //char * deque pop();
 int deque_pop();
 void display();
+void yalpsid();
 
 void main()
     {
@@ -32,7 +33,7 @@ void main()
             printf("\nChoose one option from the following list ...\n");
             printf("\n===============================================\n");
             //printf("\n1.Insert in Beginning\n2.Insert at last\n3.Delete from Beginning\n4.Delete from last\n5.Search\n6.Show\n7.Exit\n");
-            printf("\nEnter your choice? 1-push 6-display 7-exit\n");
+            printf("\nEnter your choice? 1-push 2-pop 6-display-7 8-exit\n");
             scanf("\n%d",&choice);
             switch(choice)
             {
@@ -40,7 +41,7 @@ void main()
                 dummy_deque_push();
                 break;
                 case 2:
-                //dummy_deque_pop();
+                dummy_deque_pop();
                 break;
                 case 3:
 
@@ -55,6 +56,9 @@ void main()
                 display();
                 break;
                 case 7:
+                yalpsid();
+                break;
+                case 8:
                 exit(0);
                 break;
                 default:
@@ -101,8 +105,8 @@ void dummy_deque_push()//at start
         node_creating = (struct node *) malloc(sizeof(struct node));
         printf("\nUser, please enter some int value\t");
         scanf("%d", &userinput);
-
-        if (NULL != HEAD && NULL != TAIL && NULL != HEAD -> next/* && NULL == TAIL -> prev*/) //non-second
+/*
+        if (NULL != HEAD && NULL != TAIL && NULL != HEAD -> next/* && NULL == TAIL -> prev) //non-second
         {
                 printf("\n~~~non-first~~~");
                 struct node * prevHEAD = HEAD;
@@ -118,18 +122,20 @@ void dummy_deque_push()//at start
                 printf("%d",node_creating->dummy);
                 printf("\tgo 2nd next\n");
         }
-
-        if (NULL != HEAD && NULL != TAIL && NULL == HEAD -> next/* && NULL == TAIL -> prev*/) //non-first
+*/
+        if (NULL != HEAD && NULL != TAIL/* && NULL == HEAD -> next && NULL == TAIL -> prev*/) //non-first
         {//this block is STRICTLY first
                 printf("\n~~~non-first~~~");
-                struct node * prevHEAD = HEAD;
+                //struct node * prevHEAD = HEAD;
+                //prevHEAD -> prev = NULL;
 
-
+                HEAD -> prev = node_creating;
                 node_creating -> prev = NULL;
-                node_creating -> next = prevHEAD;
+                node_creating -> next = /*prev*/HEAD;
 
 
                 node_creating -> dummy = userinput;
+
                 HEAD = node_creating;
                 printf("\nshow 2nd value=\t");
                 printf("%d",node_creating->dummy);
@@ -180,12 +186,23 @@ char * deque_pop()
 
 }
 */
-/*
+
 int dummy_deque_pop()
 {
         struct node * node_destroying;
         int returning_value;
-        if (!HEAD && !TAIL) //Do nothing
+
+        printf("\nlets print head");
+        printf("%d\n", HEAD -> dummy);
+        printf("\nlets print TAIL");
+        printf("%d\n", TAIL -> dummy);
+        printf("Now returning value");
+        printf("%d\n", node_destroying -> dummy);
+        printf("Now prev!!!!!!!!!!!!!!!!!!!!!");
+        printf("%d\n", node_destroying -> prev);
+        printf("\nZBS!");
+
+        if (!HEAD && !TAIL) printf("\nlets print head"); //Do nothing
         if (NULL != HEAD && NULL != TAIL && HEAD == TAIL)//1 item case
         {
                 node_destroying = TAIL;
@@ -205,7 +222,7 @@ int dummy_deque_pop()
         }
         return returning_value;
 }
-*/
+
 void display()
 {
     printf("BEFORE ENTERING DISPLAY");
@@ -217,6 +234,8 @@ void display()
         printf("%d\n", TAIL -> dummy);
         printf("Now iterator");
         printf("%d\n", iterator -> dummy);
+        printf("Now prevprevprevprev");
+        printf("%d\n", iterator -> prev);
         printf("\nZBS!");
         if(NULL == HEAD)
         {
@@ -231,6 +250,38 @@ void display()
 
                 printf("%d\n", iterator -> dummy);
                 iterator = iterator -> next;
+            }
+            printf("%d\n", iterator -> dummy);
+            printf("else");
+        }
+        printf("after else (QUITTING DISPLAY)");
+
+}
+void yalpsid()
+{
+    printf("BEFORE ENTERING DISPLAY");
+        struct node *iterator;
+        iterator = TAIL;
+        printf("\nlets print head");
+        printf("%d\n", HEAD -> dummy);
+        printf("\nlets print TAIL");
+        printf("%d\n", TAIL -> dummy);
+        printf("Now iterator");
+        printf("%d\n", iterator -> dummy);
+        printf("\nZBS!");
+        if(NULL == TAIL)
+        {
+            printf("\nnothing to print");
+        }
+        else
+        {
+            printf("\n printing values ... \n");
+
+            while(NULL != iterator && NULL != iterator -> prev)
+            {
+
+                printf("%d\n", iterator -> dummy);
+                iterator = iterator -> prev;
             }
             printf("%d\n", iterator -> dummy);
             printf("else");
